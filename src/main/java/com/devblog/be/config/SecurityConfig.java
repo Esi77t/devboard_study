54d519cc4a8d806b4897340d164d5897b92d66cf
Esi77t <2089651@naver.com>
2025-07-17 17:49:46 +0900

    commit

--- src/main/java/com/devblog/be/config/SecurityConfig.java
+++ src/main/java/com/devblog/be/config/SecurityConfig.java
@@ -41,8 +41,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
-                "https://d3nwp35bq2qg87.cloudfront.net",
-                "https://myaccountbook.store"
+                "http://3.35.8.210:10000",
+                "http://my-devblog-front.s3-website.ap-northeast-2.amazonaws.com",
+                "https://myaccountbook.shop"
         )); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
