b970f54847e235f5ef99908439833789d06816a6
Esi77t <2089651@naver.com>
2025-07-21 15:36:56 +0900

    lb-last?

--- src/main/java/com/devblog/be/config/SecurityConfig.java
+++ src/main/java/com/devblog/be/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
 		http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
 		http.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers("/").permitAll()
+				.requestMatchers("/health").permitAll()
 				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
 				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
